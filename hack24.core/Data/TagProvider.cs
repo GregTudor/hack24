2d2f586bd323b8dd4ed2a2be3e3c26cc2e7b8d30
Matt Holland <matt.holland90@gmail.com>
2016-03-19 16:34:48 +0000

    Typos

--- hack24.core/Data/TagProvider.cs
+++ hack24.core/Data/TagProvider.cs
@@ -8,7 +8,7 @@ namespace hack24.core.Data
 		public IEnumerable<Tag> Locations = new[]
 		{
 			new Tag("unitedkingdom", "United Kingdom"),
-			new Tag("austrailia", "Austrailia"),
+			new Tag("austrailia", "Australia"),
 			new Tag("unitedstatesofamerica", "United States of America"),
 			new Tag("nottingham", "Nottingham"),
 			new Tag("newyork", "New York"),
@@ -29,18 +29,18 @@ namespace hack24.core.Data
 		public IEnumerable<Tag> Skills = new[]
 		{
 			new Tag("problemsolver", "Problem solver"),
-			new Tag("listener", "Problem solver"),
-			new Tag("communicator", "Problem solver"),
+			new Tag("listener", "Listener"),
+			new Tag("communicator", "Communicator"),
 			new Tag("strategist", "Strategist"),
 			new Tag("risktaker", "Risk taker"),
-			new Tag("descisionmaker", "Decision maker"),
+			new Tag("descisionmaker", "Decision Maker"),
 			new Tag("collaborator", "Collaborator"),
 			new Tag("planner", "Planner"),
 			new Tag("organiser", "Organiser"),
 			new Tag("planner", "Planner"),
-			new Tag("briefmaker", "Brief maker"),
-			new Tag("officesuplier", "officesupplier"),
-			new Tag("creativeproblemsolver", "Creative problem solver")
+			new Tag("briefmaker", "Brief Maker"),
+			new Tag("officesuplier", "Office Supplier"),
+			new Tag("creativeproblemsolver", "Creative Problem Solver")
 		};
 	}
 }
