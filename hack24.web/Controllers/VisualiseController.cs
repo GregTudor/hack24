2e3e5dfdeb08146c22980170a2f40fd0e5401920
Greg Tudor <greg@gregtudor.co.uk>
2016-03-19 21:39:31 +0000

    visualise controller

---
+++ hack24.web/Controllers/VisualiseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hack24.web.Controllers
+{
+    public class VisualiseController : Controller
+    {
+        // GET: Visualise
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
