f297790d9b95944ef0b8caba6b5bc0c454b1e4e4
Greg Tudor <greg@gregtudor.co.uk>
2016-03-19 13:54:16 +0000

    mvc

---
+++ hack24.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hack24.web.Startup))]
+namespace hack24.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
