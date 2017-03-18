1626e4a57054c450c5263fadaf7d64c6cfa83dc1
Matt Holland <matt.holland@myunidays.com>
2017-03-18 13:20:31 +0000

    First attempt at some inbox polling code

--- Engageatron/Listener/Program.cs
+++ Engageatron/Listener/Program.cs
@@ -1,15 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
 
 namespace Listener
 {
 	class Program
 	{
+		static string[] Scopes = { GmailService.Scope.GmailReadonly };
+		static string ApplicationName = "Gmail API .NET Quickstart";
+
 		static void Main(string[] args)
 		{
+			UserCredential credential;
+
+			using (var stream =
+				new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+			{
+				string credPath = System.Environment.GetFolderPath(
+					System.Environment.SpecialFolder.Personal);
+				credPath = Path.Combine(credPath, ".credentials/gmail-engageatron.json");
+
+				credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+					GoogleClientSecrets.Load(stream).Secrets,
+					Scopes,
+					"user",
+					CancellationToken.None,
+					new FileDataStore(credPath, true)).Result;
+				Console.WriteLine("Credential file saved to: " + credPath);
+			}
+
+			var service = new GmailService(new BaseClientService.Initializer
+			{
+				HttpClientInitializer = credential,
+				ApplicationName = ApplicationName,
+			});
+
+			var messages = new List<Message>();
+			var request = service.Users.Messages.List("me");
+			request.Q = "is:unread";
+
+			ListMessagesResponse response = request.Execute();
+			messages.AddRange(response.Messages);
 		}
 	}
 }
