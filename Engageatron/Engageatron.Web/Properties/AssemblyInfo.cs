f96890c0990cd64e2facba1037af0322f104e47d
Greg Tudor <Greg@Myunidays.com>
2017-03-18 23:06:43 +0000

    nancy

--- Engageatron/Engageatron.Web/Properties/AssemblyInfo.cs
+++ Engageatron/Engageatron.Web/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Engageatron")]
+[assembly: AssemblyTitle("Engageatron.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Engageatron")]
+[assembly: AssemblyProduct("Engageatron.Web")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7f105d56-6abf-4bc5-88c6-df07220da452")]
+[assembly: Guid("b9962103-4f11-4632-8c83-80174582aafb")]
 
 // Version information for an assembly consists of the following four values:
 //
